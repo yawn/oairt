2855c78db4ee145010f47506326f90b2e8b9c00e
Joern Barthel <joern@barthel.digital>
2024-10-04 20:51:21 +0200

    fix: changed optionals to mandatory, convert into error

--- error_server.go
+++ error_server.go
@@ -1,5 +1,7 @@
 package oairt
 
+import "fmt"
+
 const TypeServerError Type = "error"
 
 type ServerError struct {
@@ -8,12 +10,25 @@ type ServerError struct {
 	Details *ServerErrorDetails `json:"error"`
 }
 
+func (s *ServerError) Error() string {
+
+	details := s.Details
+
+	return fmt.Sprintf("%s (%s): %s (%#v, %s)",
+		details.Type,
+		details.Code,
+		details.Message,
+		details.Param,
+		details.EventID,
+	)
+}
+
 func (s *ServerError) isServer() {}
 
 type ServerErrorDetails struct {
-	Type    string  `json:"type"`
-	Code    *string `json:"code,omitempty"`
-	Message string  `json:"message"`
-	Param   *string `json:"param,omitempty"`
-	EventID *string `json:"event_id,omitempty"`
+	Type    string `json:"type"`
+	Code    string `json:"code,omitempty"`
+	Message string `json:"message"`
+	Param   string `json:"param,omitempty"`
+	EventID string `json:"event_id,omitempty"`
 }
